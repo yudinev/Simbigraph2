8353b8a611eb4f7dcf89c1bb639443b2a2c3c2f2
Bitluck <bitluck11@gmail.com>
2018-05-28 04:25:23 +0600

    Add converters to pajek and edges array formats

--- src/simbigraph/stat/algorithms/cluster/louvain/FormatConverter.java
+++ src/simbigraph/stat/algorithms/cluster/louvain/FormatConverter.java
@@ -1,19 +1,55 @@
 package simbigraph.stat.algorithms.cluster.louvain;
 
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import edu.uci.ics.jung.graph.util.Pair;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class FormatConverter<E> {
-    public Edge[] convertFromJungToEdgeFormat(Graph<Number, E> g) {
-        Edge[] edges = new Edge[g.getEdgeCount()];
+    public static final double EDGE_WEIGHT = 1.0;
+
+    public Graph<Number, Number> convertToPajekFormat(Graph<?, E> graph) {
+        Graph g = new UndirectedSparseGraph<Number, Number>();
+
+        Map uniqueNodes = new HashMap<Object, Number>();
 
         int i = 0;
-        for (E e:g.getEdges()) {
+        Iterator itNodes = graph.getVertices().iterator();
+        while (itNodes.hasNext()) {
+            uniqueNodes.put(itNodes.next(), i);
+            g.addVertex(i);
+            ++i;
+        }
+
+
+        for (E e : graph.getEdges()) {
+            Pair<?> p = graph.getEndpoints(e);
+            g.addEdge(e, (Number) uniqueNodes.get(p.getFirst()), (Number) uniqueNodes.get(p.getSecond()));
+        }
+
+        return g;
+    }
+
+    public Edge[] convertToEdgeFormat(Graph<?, E> graph) {
+        Edge[] edges = new Edge[graph.getEdgeCount()];
+
+        Map uniqueNodes = new HashMap<Object, Number>();
+
+        Iterator itNodes = graph.getVertices().iterator();
+        for(int i = 0; itNodes.hasNext(); ++i) {
+            uniqueNodes.put(itNodes.next(), i);
+        }
+
+        int i = 0;
+        for(E e : graph.getEdges()) {
             edges[i] = new Edge();
-            Pair<Number> p = g.getEndpoints(e);
-            edges[i].v = (int)p.getFirst();
-            edges[i].weight = 1.0;
-            edges[i].next = (int)p.getSecond();
+            Pair<?> p = graph.getEndpoints(e);
+            edges[i].v = (int)uniqueNodes.get(p.getFirst());
+            edges[i].weight = EDGE_WEIGHT;
+            edges[i].next = (int)uniqueNodes.get(p.getSecond());
             ++i;
         }
 
